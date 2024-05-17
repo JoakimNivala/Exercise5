3abfcc9c21fed98ec5d9c169e787919259b6c5f7
JoakimNivala <joakim.nivala@edu.lapinamk.fi>
2024-05-17 23:03:58 +0300

    poerhaps now

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from textblob import TextBlob
 from flask_cors import CORS
 
@@ -9,7 +9,8 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 def analyze_sentiment():
     if request.method == "POST":
         try:
-            text = request.form["text"] 
+            data = request.get_json()
+            text = data["text"] 
             sentiment = TextBlob(text).sentiment.polarity
 
             if sentiment > 0:
@@ -21,7 +22,7 @@ def analyze_sentiment():
 
             return result
         except KeyError:
-            return "Error: 'text' key not found in form data"
+            return "Error: 'text' key not found in request data", 400
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
