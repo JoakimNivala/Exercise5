1c0238b3aace8aead66638c9bb43fd70a345ffe5
JoakimNivala <joakim.nivala@edu.lapinamk.fi>
2024-05-17 22:21:54 +0300

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("https://be-cloud-computing-course-project.rahtiapp.fi/", methods=["GET", "POST"])
 def analyze_sentiment():
   if request.method == "POST":
     text = request.form["text"] 
