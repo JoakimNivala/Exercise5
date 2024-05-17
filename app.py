240a5a8a82b72664cd7658cb7b134b44793b2b33
JoakimNivala <joakim.nivala@edu.lapinamk.fi>
2024-05-17 22:11:06 +0300

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("*", methods="POST")
 def analyze_sentiment():
   if request.method == "POST":
     text = request.form["text"] 
